48b95a5014fee8195faedafcee7e0295f6c48a64
Hugo R Alves <r.hugoalves66@gmail.com>
2022-07-12 20:41:47 -0300

    Variáveis

---
+++ Tipos-Variaveis-Funcoes/Variaveis.js
@@ -0,0 +1,12 @@
+const forma = 'retangulo'; 
+let altura = 3; 
+let comprimento = 9; 
+let area; 
+
+if(forma === 'retangulo'){
+    area = altura * comprimento; 
+    console.log(area)
+} else{
+    console.log("Não é um retângulo")
+}
+
