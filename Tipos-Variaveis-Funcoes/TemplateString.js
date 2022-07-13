7692e068e70f83991cf32b85281a975d5b9562e3
Hugo R Alves <r.hugoalves66@gmail.com>
2022-07-12 22:24:45 -0300

    TemplateString

---
+++ Tipos-Variaveis-Funcoes/TemplateString.js
@@ -0,0 +1,6 @@
+const nome = "Adapak"; 
+const apelido = "Espadachim de carvão"; 
+const curiosidade = "Filho de um deus"; 
+
+const apresentacao = `Meu nome é ${nome}, mas me conhecem como ${apelido}. Também sou ${curiosidade}`; 
+console.log(apresentacao)
