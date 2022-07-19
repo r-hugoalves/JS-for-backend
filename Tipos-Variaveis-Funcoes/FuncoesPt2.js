9bdb77f0a6104fdbd785ee710aa7197468cf0c45
Hugo R Alves <r.hugoalves66@gmail.com>
2022-07-18 22:16:17 -0300

    Expressão de funções

--- Tipos-Variaveis-Funcoes/FuncoesPt2.js
+++ Tipos-Variaveis-Funcoes/FuncoesPt2.js
@@ -20,3 +20,14 @@ function multiplicacao(numero1, numero2){
 }
 
 console.log(multiplicacao(soma(2,3), soma(4,5)));  
+
+
+            //Expressão de função 
+
+//A declaração da função é anônima, quem recebe o nome é a variável
+
+    //Isso tem uma implicancia, e que é a principal diferença entre a expressão de função e a declaração de função
+        //Como ela se comporta como uma variável, eu só posso chama-la, depois que ela já tiver sido inicializada! 
+        
+const soma = function (valor1, valor2) { return valor1 + valor2}; 
+console.log(soma(3,7)); 
